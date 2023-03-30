61ce701494be106e4927ddbbe9b22aa7fb8ade93
FelipeBriosso <fbriosso@hotmail.com>
2023-03-30 19:42:44 -0300

    [Green] clientSignUp

---
+++ Client/App/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd
+{
+    public class Client
+    {
+        public Client()
+        {
+        }
+       public String name { get; set; }
+       public String password { get; set; }
+
+    }
+}
---
+++ Client/ClientTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using BackEnd;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Security.Cryptography;
+
+namespace ClientTest
+{
+    [TestClass]
+    public class ClientTest
+    {
+        [TestMethod]
+        public void successfulClientSignUp()
+        {
+            var cl = new Client()
+            {
+                name = "Joe",
+                password="1234",
+            };
+        Assert.AreEqual(cl.name,"Joe");
+        Assert.AreEqual(cl.password, "1234");
+        }
+    }
+}
