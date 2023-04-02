521a22371cb63a8969083e67e0abc2f6c14b0466
nuhalde <nicoleuhalde@gmail.com>
2023-04-02 19:21:31 -0300

    [RED] Figure test

---
+++ Figure/Figure.cs
@@ -0,0 +1,51 @@
+﻿using App;
+using BackEnd;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figure
+{
+    public abstract class Figure
+    {
+        private const int nameMinimumLength = 3;
+        private const int nameMaximumLength = 20;
+        private String name;
+        private Client client;
+
+        public Client ClientRegistraction
+        { 
+            get => client;
+            set => client = value;
+        }
+
+        public String Name
+        {
+            get => name;
+            set
+            {
+                if (isAValidName(value))
+                {
+                    name = value;
+                }
+            }
+
+        }
+        private bool isAValidName(String value)
+        {
+            if (!value.All(char.IsLetterOrDigit))
+            {
+                throw new BackEndException("Name must be alphanumeric");
+            }
+            if (value.Length < nameMinimumLength || value.Length > nameMaximumLength)
+            {
+                throw new BackEndException("Name length must be between 3 and 20");
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Figure/FigureTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FigureTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FigureTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("345ccc16-17c3-4618-b664-4d3d240edfd3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
