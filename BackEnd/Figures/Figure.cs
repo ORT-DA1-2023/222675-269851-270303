04d5a229de19e3f225130f633d8c7ae10d6794ec
FelipeBriosso <fbriosso@hotmail.com>
2023-04-17 19:01:37 -0300

    almost done folder fixing

---
+++ BackEnd/Figures/Figure.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Render3D.BackEnd.Figures
+{
+    public abstract class Figure 
+    {
+        private String name;
+        private Client client;
+
+
+        public virtual Client Client
+        {
+            get => client;
+            set => client = value;
+        }
+
+        public virtual String Name
+        {
+            get => name;
+            set
+            {
+                if (isAValidName(value))
+                {
+                    name = value;
+                }
+            }
+
+        }
+
+        protected bool isAValidName(String value)
+        {
+            if (value.Length == 0) throw new BackEndException("The name must not be empty");
+            if (value.Trim().Length != value.Length) throw new BackEndException("Name must not start or end with spaces");
+            return true;
+        }
+    }
+}
---
+++ UnitTest/LambertianTest.cs
@@ -0,0 +1,88 @@
+﻿using Render3D.BackEnd;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Render3D.BackEnd.Materials;
+
+namespace Render3D.UnitTest
+{
+    [TestClass]
+    public class LambertianTest
+    {
+        private LambertianMaterial materialSample;
+        private string validMaterialName = "LambertianMaterialName";
+        private int[] colorLowerThan = { -1, -1, -1 };
+        private int[] colorGreaterThan = { 266, 266, 266 };
+        private int[] validColor = { 15, 15, 15 };
+
+        private Client clientSample;
+        private string clientSampleName = "clientSampleName";
+
+
+        [TestInitialize]
+        public void initialize()
+        {
+            clientSample = new Client()
+            {
+                Name = clientSampleName
+            };
+            materialSample = new LambertianMaterial();
+            materialSample.Client = clientSample;
+
+        }
+        [TestMethod]
+        public void givenAValidNameItAssignsItToTheLambertarianMaterial()
+        {
+            Assert.IsNotNull(materialSample);
+            materialSample.Name = validMaterialName;
+            Assert.AreEqual(validMaterialName,materialSample.Name);
+            
+        }
+        [TestMethod]
+        [ExpectedException(typeof(BackEndException), "Name must not be empty")]
+        public void givenAnEmptyNameItThrowsABackEndException()
+        {
+            materialSample.Name = "";
+        }
+        [TestMethod]
+        [ExpectedException(typeof(BackEndException), "Name cant start or end with space")]
+        public void givenANameThatStartsWithSpaceItThrowsABackEndException()
+        {
+            materialSample.Name = " "+validMaterialName;
+        }
+        [TestMethod]
+        [ExpectedException(typeof(BackEndException), "Name cant start or end with space")]
+        public void givenANameThatEndsWithSpaceItThrowsABackEndException()
+        {
+            materialSample.Name =validMaterialName + " ";
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(BackEndException), "color cant be lower than 0 or greater than 255")]
+        public void givenASmallerThanPossibleRGBItThrowsABackendException()
+        { 
+            ((LambertianMaterial)materialSample).Color=colorLowerThan;
+        }
+        [TestMethod]
+        [ExpectedException(typeof(BackEndException), "color cant be lower than 0 or greater than 255")]
+        public void givenABiggerThanPossibleRGBItThrowsABackendException()
+        {
+            ((LambertianMaterial)materialSample).Color = colorGreaterThan;
+        }
+        [TestMethod]
+        public void givenAValidRGBItAssignsItToTheMaterial()
+        {
+            materialSample.Color = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                materialSample.Color[i] = validColor[i];
+                Assert.IsTrue(materialSample.Color[i] == validColor[i]);
+            }
+        }
+        [TestMethod]
+        public void givenAMaterialItReturnsItsClient()
+        {
+            Assert.AreEqual(materialSample.Client, clientSample);
+        }
+
+    }
+}
