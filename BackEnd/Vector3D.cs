5995690f7e5244b4a3044d982c03714d270cb498
FelipeBriosso <fbriosso@hotmail.com>
2023-04-20 16:57:36 -0300

    [green] vector class initialize

---
+++ BackEnd/Vector3D.cs
@@ -0,0 +1,19 @@
+﻿namespace Render3D.BackEnd
+{
+    public class Vector3D
+    {
+        private int _x;
+        private int _y;
+        private int _z;
+
+        public Vector3D(int v1, int v2, int v3)
+        {
+            this._x = v1;
+            this._y = v2;
+            this._z = v3;
+        }
+        public int X { get=> _x; }  
+        public int Y { get => _y;}
+        public int Z { get => _z;}
+    }
+}
---
+++ UnitTest/VectorTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Render3D.BackEnd.Figures;
+using Render3D.BackEnd;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Render3D.UnitTest
+{
+    
+    [TestClass]
+    public class VectorTest
+    {
+        private const int one = 1;
+        private Vector3D allOnes;
+        [TestInitialize]
+        public void initialize()
+        {
+            allOnes = new Vector3D(1, 1, 1);
+        }
+       
+
+        [TestMethod]
+        public void givenAVectorItAssignsTheCoordinates()
+        {
+          Assert.AreEqual(allOnes.X, one);
+          Assert.AreEqual(allOnes.Y, one);
+          Assert.AreEqual(allOnes.Z, one);
+        }
+    }
+}
