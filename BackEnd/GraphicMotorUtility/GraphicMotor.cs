05a6f0e4d998c6bcd9e57793378bfae6d009f26c
nuhalde <nicoleuhalde@gmail.com>
2023-04-22 21:16:21 -0300

    [GREEN] GraphicMotor class and Resolution test

---
+++ BackEnd/GraphicMotorUtility/GraphicMotor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Render3D.BackEnd.GraphicMotorUtility
+{
+    public class GraphicMotor
+    {
+        private int _resolution;
+
+        public int Resolution
+        {
+
+            get { return _resolution; }
+            set
+            {
+                if (IsAValidResolution(value))
+                {
+                    _resolution = value;
+                }
+            }
+        }
+
+        private bool IsAValidResolution(int value)
+        {
+            if (value<=0)
+            {
+                throw new BackEndException("The resolution must be greater than 0.");
+            }
+            return true;
+        }
+
+    }
+}
---
+++ UnitTest/GaphicMotorTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Render3D.BackEnd;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Render3D.BackEnd.GraphicMotorUtility;
+
+namespace Render3D.UnitTest
+{
+    [TestClass]
+    public class GraphicMotorTest
+    {
+        private GraphicMotor graphicMotorSample;
+        private int resolutionSample = 300;
+        private int NegativeResolutionSample = -1;
+
+        [TestInitialize]
+        public void initialize()
+        {
+            graphicMotorSample = new GraphicMotor() { Resolution = resolutionSample};
+        }
+
+        [TestMethod]
+        public void givenAValidResolutionItAssignsItToTheGraphicMotor()
+        {
+            graphicMotorSample.Resolution = resolutionSample;
+            Assert.AreEqual(resolutionSample, graphicMotorSample.Resolution);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(BackEndException), "The resolution must be greater than 0.")]
+        public void givenANegativeResolutionItThrowsABackEndException()
+        {
+            graphicMotorSample.Resolution = NegativeResolutionSample;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Agregar aquí la lógica de las pruebas
+            //
+        }
+    }
+}
