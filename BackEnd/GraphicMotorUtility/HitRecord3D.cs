5978a270c1dc31a8fe9bb2e74e2d9aa92567ce82
nuhalde <nicoleuhalde@gmail.com>
2023-04-29 20:08:01 -0300

    [RED] new class HitRecord3D

---
+++ BackEnd/GraphicMotorUtility/HitRecord3D.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Render3D.BackEnd.GraphicMotorUtility
+{
+    public class HitRecord3D
+    {
+    }
+}
---
+++ UnitTest/HitRecord.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Render3D.BackEnd.GraphicMotorUtility;
+using System;
+
+namespace Render3D.UnitTest
+{
+    [TestClass]
+    public class HitRecord
+    {
+        private HitRecord3D hitRecordSample;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
