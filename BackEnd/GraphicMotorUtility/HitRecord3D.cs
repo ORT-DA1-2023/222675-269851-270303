97668e1bf6d8f3892fdf7079827132b5c55130a1
nuhalde <nicoleuhalde@gmail.com>
2023-04-29 20:28:20 -0300

    [GREEN] test from class HitRecord3D

--- BackEnd/GraphicMotorUtility/HitRecord3D.cs
+++ BackEnd/GraphicMotorUtility/HitRecord3D.cs
@@ -8,5 +8,41 @@ namespace Render3D.BackEnd.GraphicMotorUtility
 {
     public class HitRecord3D
     {
+        public double _module;
+        public Vector3D _intersection;
+        public Vector3D _normal;
+
+        public HitRecord3D()
+        {
+            _module = 0;
+            _intersection = new Vector3D(0, 0, 0);
+            _normal = new Vector3D(0, 0, 0);
+        }
+
+        public HitRecord3D(double module, Vector3D intersection, Vector3D normal)
+        { 
+            _module = module;
+            _intersection = intersection;
+            _normal = normal;
+        }
+
+        public double Module
+        {
+            get => _module;
+            set => _module = value;
+        }
+
+        public Vector3D Intersection
+        {
+            get => _intersection;
+            set => _intersection = value;
+        }
+
+        public Vector3D Normal
+        {
+            get => _normal;
+            set => _normal = value;
+        }
+        
     }
 }
---
+++ UnitTest/HitRecordTest.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Render3D.BackEnd;
+using Render3D.BackEnd.GraphicMotorUtility;
+using System;
+
+namespace Render3D.UnitTest
+{
+    [TestClass]
+    public class HitRecordTest
+    {
+        private HitRecord3D hitRecordSample;
+        private double moduleSample;
+        private Vector3D intersectionSample;
+        private Vector3D normalSample;
+
+        [TestInitialize]
+        public void initialize()
+        {
+            hitRecordSample = new HitRecord3D();
+        }
+
+        [TestMethod]
+        public void givenValidAtributesItAssingsToTheHitRecord()
+        {
+            moduleSample = 1;
+            intersectionSample = new Vector3D(0, 0, 0);
+            normalSample = new Vector3D(0, 0, 0);
+            hitRecordSample = new HitRecord3D(moduleSample, intersectionSample, normalSample);
+            Assert.AreEqual(moduleSample, hitRecordSample.Module);
+            Assert.AreEqual(intersectionSample, hitRecordSample.Intersection);
+            Assert.AreEqual(normalSample, hitRecordSample.Normal);
+        }
+
+        [TestMethod]
+        public void givenAValidModuleItAssingsToTheHitRecord()
+        {
+            moduleSample = 1;
+            hitRecordSample.Module = moduleSample;
+            Assert.AreEqual(moduleSample, hitRecordSample.Module);
+        }
+
+        [TestMethod]
+        public void givenAValidIntersectionItAssingsToTheHitRecord()
+        {
+            intersectionSample = new Vector3D(0, 0, 0);
+            hitRecordSample.Intersection = intersectionSample;
+            Assert.AreEqual(intersectionSample, hitRecordSample.Intersection);
+        }
+
+        [TestMethod]
+        public void givenAValidNormalItAssingsToTheHitRecord()
+        {
+            normalSample = new Vector3D(0, 0, 0);
+            hitRecordSample.Normal = normalSample;
+            Assert.AreEqual(normalSample, hitRecordSample.Normal);
+        }
+    }
+}
