ba35643b47e8105635f8fa44ec8ffb8f69197bbc
nuhalde <93264588+nuhalde@users.noreply.github.com>
2023-06-07 09:29:44 -0300

    [GREEN] fixing some things in the iu

--- UserInterface/Panels/SceneCreation.cs
+++ UserInterface/Panels/SceneCreation.cs
@@ -92,17 +92,18 @@ namespace UserInterface.Panels
             }
         }
 
+        public bool IsValidFormatAperture(string input)
+        {
+            Regex vectorFormat = new Regex(@"^\d+,\d+$");
+            return vectorFormat.IsMatch(input);
+        }
+
         public bool IsValidFormat(string input)
         {
             Regex vectorFormat = new Regex(@"^\(\s*-?\d+(\,\d+)?\s*;\s*-?\d+(\,\d+)?\s*;\s*-?\d+(\,\d+)?\s*\)$");
             return vectorFormat.IsMatch(input);
         }
 
-        public bool IsValidFormatAperture(string input)
-        {
-            Regex vectorFormat = new Regex(@"^(\d+(\.\d+)?),(\d+(\.\d+)?)$");
-            return vectorFormat.IsMatch(input);
-        }
 
         private void BtnGoBack_Click(object sender, EventArgs e)
         {
@@ -120,7 +121,15 @@ namespace UserInterface.Panels
                     {
                         if (cmbBlur.Checked)
                         {
-                            sceneController.EditCamera(scene, txtLookAt.Text, txtLookFrom.Text, (int)nrFov.Value, txtAperture.Text);
+                             if(IsValidFormatAperture(txtAperture.Text))
+                             {
+                                sceneController.EditCamera(scene, txtLookAt.Text, txtLookFrom.Text, (int)nrFov.Value, txtAperture.Text);
+                             }
+                             else
+                             {
+                                 throw new Exception("Aperture format not valid");
+                              }
+                           
                         }
                         string apertureNegative = "-1";
                         sceneController.EditCamera(scene, txtLookAt.Text, txtLookFrom.Text, (int)nrFov.Value, apertureNegative);
