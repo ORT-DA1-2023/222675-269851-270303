1c246102e4f356603208c670d31f59db007c9b1e
FelipeBriosso <112712605+FelipeBriosso@users.noreply.github.com>
2023-06-12 09:26:52 -0300

    log repo done

--- renderRepository/entities/LogEntity.cs
+++ renderRepository/entities/LogEntity.cs
@@ -8,11 +8,11 @@ namespace renderRepository.entities
     {
         [Key]
         public int Id { get; set; }
-        public ClientEntity Client { get; set; }
+        public ClientEntity ClientEntity { get; set; }
         public int RenderTimeInSeconds { get; set; }
         public DateTime RenderDate { get; set; }
         public string TimeWindowSinceLastRender { get; set; }
-        public SceneEntity Scene { get; set; }
+        public SceneEntity SceneEntity { get; set; }
         public int NumberElementsInScene { get; set; }
         public string Name { get; set; }
 
